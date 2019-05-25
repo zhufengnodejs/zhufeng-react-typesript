6fa047a62304d52153a5cdf7407a1524cacc3100
zhufengnodejs <zhufengnodejs@126.com>
2019-05-25 23:34:14 +0800

    init

---
+++ src/components/Counter.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+export interface IProps{
+  name: string
+}
+export interface IState{
+  number: number,
+  amount:number
+}
+export default class Counter extends React.Component<IProps,IState>{
+  state = {number:0,amount:0}
+  handleChange = (event:React.ChangeEvent<HTMLInputElement>)=>{
+    this.setState({amount:parseInt(event.target.value)});
+  }
+  render() {
+    const {name}=this.props;
+    const {number,amount}=this.state;
+    const container:React.CSSProperties = {
+      color:'green',
+      border:'1px solid red'
+    }
+    return (
+      <div style={container}>
+        <p>{name}:{number}</p>
+        <input value={amount} onChange={event=>this.handleChange(event)}/>
+        <button onClick={()=>this.setState({number:number+amount})}>+</button>
+      </div>
+    )
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Counter from './components/Counter';
+ReactDOM.render((
+  <Counter name="计数器"/>
+),document.getElementById('root'));
